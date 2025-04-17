c4e3ab253ab494df7d3d95a6d485d10aa61c8e94
hall9zeha <vmwaretars@gmail.com>
2025-04-17 13:53:21 -0500

    Created PlayerScreen

---
+++ app/src/main/java/com/barryzeha/kmusic/ui/theme/screens/PlayerScreen.kt
@@ -0,0 +1,142 @@
+package com.barryzeha.kmusic.ui.theme.screens
+
+import android.annotation.SuppressLint
+import android.view.RoundedCorner
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Slider
+
+import androidx.compose.material3.SliderDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.barryzeha.kmusic.R
+import org.intellij.lang.annotations.JdkConstants
+import java.nio.file.WatchEvent
+
+/****
+ * Project KMusic
+ * Created by Barry Zea H. on 17/04/25.
+ * Copyright (c)  All rights reserved.
+ ***/
+@Composable
+fun PlayerScreen() {
+    Box(Modifier.fillMaxSize().padding(8.dp)){
+        Body()
+    }
+}
+
+@Composable
+fun Body(){
+    Column(modifier= Modifier.fillMaxSize().padding(top=48.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        CoverAlbumArt()
+        SongDescription()
+    }
+}
+@Composable
+fun CoverAlbumArt(){
+    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+    Card(
+        modifier = Modifier.padding(top = 48.dp)
+            /*.aspectRatio(1f)*//* aspect ratio square 1:1 */
+            .size(screenWidth * 0.8f) /* For control the square size we choose this option */
+            /*.fillMaxWidth(0.8f)*/,
+        elevation= CardDefaults.cardElevation(4.dp),
+        border=_root_ide_package_.androidx.compose.foundation.BorderStroke(0.dp, Color.Transparent),
+        shape = RoundedCornerShape(16.dp),
+
+    ) {
+        Image(
+            modifier=Modifier.fillMaxSize(),
+            painter = painterResource(id= R.drawable.ic_launcher_background),
+            contentDescription = "Cover art")
+    }
+}
+
+@Composable
+fun SongDescription(){
+    Column(modifier= Modifier.padding(start = 8.dp, top = 16.dp, end = 8.dp, bottom = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text="Song title", fontSize = 18.sp)
+        Text(text = "Artist", fontSize = 16.sp)
+        Text(text="Album", fontSize = 16.sp)
+        Seekbar()
+    }
+}
+@SuppressLint("UnrememberedMutableInteractionSource")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Seekbar(){
+    var sliderValue by remember { mutableFloatStateOf(0f) }
+    val interactionsource = MutableInteractionSource()
+    Column(modifier=Modifier.fillMaxWidth(). padding(top = 24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Slider(modifier= Modifier.fillMaxWidth()
+            ,value = sliderValue,
+            onValueChange = { newValue ->
+                sliderValue = newValue
+            },
+            valueRange = 0f..1000f, steps = 999,
+            thumb = {
+                SliderDefaults.Thumb(
+                    interactionSource = interactionsource,
+                    thumbSize = DpSize(24.dp,24.dp)
+                )
+            },
+            track = {sliderState->
+
+                SliderDefaults.Track(
+                    modifier = Modifier.height(4.dp),
+                    drawStopIndicator = {},
+                    colors = SliderDefaults.colors(
+                        activeTickColor = Color.Transparent,
+                        inactiveTickColor = Color.Transparent,
+                        thumbColor = Color.Transparent,
+                    ),
+                    sliderState = sliderState, thumbTrackGapSize = 0.dp)
+            }
+        )
+    }
+}
+
+@Composable
+fun PlayerControls(){
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PreviewComposable(){
+    Box() {
+        Body()
+    }
+}
